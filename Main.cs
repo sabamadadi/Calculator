f44e143fa0b74cf2f43ac58a8ceb6710f8fd617c
Saba Madadi <sabamadadi9@gmail.com>
2023-08-16 12:56:02 +0330

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,7 +1,51 @@
 using System;
 
-class Program {
-    static void Main(string[] args) {
-        Console.WriteLine("Hello, world!");
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Calculator!");
+
+        while (true)
+        {
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the operator (+, -, *, /):");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator!");
+                    break;
+            }
+
+            Console.WriteLine("Result: " + result);
+            Console.WriteLine("Do you want to perform another calculation? (Y/N)");
+            string choice = Console.ReadLine();
+
+            if (choice.ToLower() != "y")
+                break;
+        }
+
+        Console.WriteLine("Thank you for using the Calculator!");
     }
 }
